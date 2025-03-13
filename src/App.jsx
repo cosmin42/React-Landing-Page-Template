785002d6b0bf6e185cc9dcf8d94de60d21eae9ba
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2025-03-13 17:44:18 +0200

    Remove the Service section.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { Features } from "./components/features";
 import { About } from "./components/about";
-import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
@@ -27,7 +26,6 @@ const App = () => {
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery} />
       <Contact data={landingPageData.Contact} />
     </div>
