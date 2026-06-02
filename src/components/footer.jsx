a1918a4517862c4495ac9f8ff4c188edc6f25455
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2026-06-02 21:17:55 +0200

    Update about image and the footer.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,12 +11,6 @@ export const Footer = () => {
   return (
     <div id="footer">
       <div className="container text-center">
-        <p>
-          {footer.creditLine} {footer.designBy}{" "}
-          <a href="http://www.templatewire.com" rel="nofollow">
-            TemplateWire
-          </a>
-        </p>
         <div className="footer-links" aria-label={footer.linksAria}>
           <Link to="/credits">{footer.credits}</Link>
           <Link to="/privacy">{footer.privacy}</Link>
